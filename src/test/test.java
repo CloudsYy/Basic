1d2bfe904bf14da6b2e26acc2bd3f8f39939e0ce
Cloud <hongzezhi_email@126.com>
2020-06-09 10:31:25 +0800

    add space

--- src/test/test.java
+++ src/test/test.java
@@ -8,8 +8,12 @@ package test;
  */
 public class test {
 
+    // 这里表明了为什么在进制转换的时候需要加&0xff的计算,
+    // 这里加&0xff和没加，前后两个的补码不一致，并且此时我们需要关
+    // 注补码的一致性，并不需要关注十进制的数据，
+    // 转成java的int的时候，对于原码，补码和反码需要去另外了解一下
     public static void main(String[] args) {
-        String string = "粤SA6666";
-        System.out.println(string);
+        System.out.println(Integer.toBinaryString(-8&0xff));
+        System.out.println(Integer.toBinaryString(-8));
     }
 }
