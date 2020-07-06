56522bc76ee4979f6e6fbc351730f02c0ba2c171
HongZeZhi <HongZeZhi>
2020-07-06 21:56:02 +0800

    冲突

--- src/test/test.java
+++ src/test/test.java
@@ -1,5 +1,10 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @program: Basic
  * @description: 只是测试而已
@@ -8,8 +13,23 @@ package test;
  */
 public class test {
 
+    /**
+     * String字符串转成List<Long>数据格式
+     * String str = "1,2,3,4,5,6" -> List<Long> listLong [1,2,3,4,5,6];
+     *
+     * @param strArr
+     * @return
+     */
+    public static List<Long> stringToLongList(String strArr) {
+        return Arrays.stream(strArr.split(","))
+                .map(s -> Long.parseLong(s.trim()))
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
-        String string = "粤SA6666";
-        System.out.println(string);
+        String str = "123,456";
+        List<Long> longList = stringToLongList(str);
+        Long a = longList.get(0);
+        System.out.println(a);
     }
 }
