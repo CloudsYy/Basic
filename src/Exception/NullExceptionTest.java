f4bdc3567760d47283d3bca7a2c0fbbe2b39245c
Cloud <hongzezhi_email@126.com>
2020-08-01 08:56:49 +0800

    java8 map.computeIfAbsent的使用和exception需要注意的点

---
+++ src/Exception/NullExceptionTest.java
@@ -0,0 +1,25 @@
+package Exception;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @program: Basic
+ * @description: 空指针异常
+ * @author: Cloud
+ * @create: 2020/7/31 14:18:58
+ */
+public class NullExceptionTest {
+
+//    public static List getList(){
+//        List<String> list = new ArrayList<>();
+//        list.add("123");
+//        return Collections.emptyList();
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(getList());
+//    }
+}
